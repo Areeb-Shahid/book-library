280c56625a98e3f5e0e495ab6f6bb6d4008e0b25
Areeb <areebshahid123@gmail.com>
2023-07-21 23:26:20 +0500

    commit updates 1

--- main.py
+++ main.py
@@ -1,30 +1,67 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+# Create the extension
+db = SQLAlchemy()
+# Initialise the app with the extension
+db.init_app(app)
+
+
+# CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+# # CREATE RECORD
+# with app.app_context():
+#     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", books=all_books)
+    books = Book.query.all()
+    return render_template("index.html", books=books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
-
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         # NOTE: You can use the redirect method from flask to redirect to another route
         # e.g. in this case to the home page after the form has been submitted.
         return redirect(url_for('home'))
 
     return render_template("add.html")
+
+@app.route("/edit_rating/<int:book_id>", method=["GET", "POST"])
+def edit_rating(book_id):
+    book = Book.query.get(book_id)
+    if book is None:
+        return render_template("book_")
+
 
 
 if __name__ == "__main__":
